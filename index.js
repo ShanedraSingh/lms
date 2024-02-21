19bd2365c0d09bdbc2b1fb2e97212e9be119638f
Shanedra Singh <Sachinthakur@email.com>
2024-02-22 05:18:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let library = [];
+
+function addBook() {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = parseInt(document.getElementById('pages').value);
+    let genre = document.getElementById('genre').value;
+
+    if (title && author && pages && genre) {
+        let book = {
+            title: title,
+            author: author,
+            pages: pages,
+            genre: genre
+        };
+        library.push(book);
+        displayBooks();
+    } else {
+        alert('Please fill in all fields.');
+    }
+}
+
+function searchBooks() {
+    let searchTitle = document.getElementById('searchTitle').value.toLowerCase();
+    let searchResult = library.filter(book => book.title.toLowerCase().includes(searchTitle));
+    displayBooks(searchResult);
+}
+
+function displayBooks(books = library) {
+    let bookList = document.getElementById('bookList');
+    bookList.innerHTML = '';
+
+    books.forEach(book => {
+        let bookDiv = document.createElement('div');
+        bookDiv.classList.add('book');
+        bookDiv.innerHTML = `
+            <h3>${book.title}</h3>
+            <p>Author: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+            <p>Genre: ${book.genre}</p>
+        `;
+        bookList.appendChild(bookDiv);
+    });
+}
